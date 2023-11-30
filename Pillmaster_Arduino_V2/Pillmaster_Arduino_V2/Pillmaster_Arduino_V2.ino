1d9c5a38d16c99591e66a5167339274602a1a1c2
TopCrunch <100889952+TopCrunch@users.noreply.github.com>
2023-11-29 16:42:53 -0800

    weight code but buggy

--- Pillmaster_Arduino_V2/Pillmaster_Arduino_V2/Pillmaster_Arduino_V2.ino
+++ Pillmaster_Arduino_V2/Pillmaster_Arduino_V2/Pillmaster_Arduino_V2.ino
@@ -1,5 +1,9 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
+#include <HX711_ADC.h>
+#if defined(ESP8266) || defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
 /*
 PillMaster Stepper Program
   running through A4988 driver board
@@ -25,11 +29,15 @@ const unsigned char cflag = 0x7;
 const unsigned char bflag = 0x6;
 const unsigned char aflag = 0x5;
 
+const unsigned char altflag = 0x4;
+const unsigned char weightflag = 0x4;
+const unsigned char tripFlag = 0x5;
+
 const int max_operations = 6;
-const int operation_medium_main[] = {  0,-50,   0, 0,  0, 50};
-const int operation_medium_adjs[] = {-20, 50, -30, 0, 50,-50};
-const int operation_small_main[] = {  0,-50,   0, 0,  0, 50};
-const int operation_small_adjs[] = {-20, 50, -30, 0, 50,-50};
+const int operation_medium_main[] = { 0, -50, 0, 0, 0, 50 };
+const int operation_medium_adjs[] = { -20, 50, -30, 0, 50, -50 };
+const int operation_small_main[] = { 0, -50, 0, 0, 0, 50 };
+const int operation_small_adjs[] = { -20, 50, -30, 0, 50, -50 };
 
 const int dirPinMain = 2;
 const int stepPinMain = 3;
@@ -46,6 +54,11 @@ const int enPinB = 7;
 const int enPinC = 8;
 const int enPinD = A0;
 const int enPinE = A1;
+
+const int weightPinDT = A2;
+const int weightPinSCK = A3;
+
+const int tripPin = A4;
 
 unsigned char currentMotor = aflag;
 
@@ -65,10 +78,11 @@ AccelStepper ajStepper2(AccelStepper::DRIVER, stepPinAdj2, dirPinAdj2);
 
 void setup() {
   Serial.begin(9600);
+  weightSetup();
   //enable pins
   pinMode(A5, OUTPUT);
   pinMode(A4, OUTPUT);
-  pinMode(A3, OUTPUT);
+  pinMode(A3, INPUT);
   pinMode(A2, OUTPUT);
   pinMode(A1, OUTPUT);
   pinMode(A0, OUTPUT);
@@ -81,6 +95,86 @@ void setup() {
 
 
   swapMotor(aflag);
+}
+
+HX711_ADC LoadCell(weightPinDT, weightPinSCK);
+const int calibrationAddr = 0;
+unsigned long time = 0;
+
+void weightSetup() {
+  LoadCell.begin();
+  float calibrationValue;
+#if defined(ESP8266) || defined(ESP32)
+  EEPROM.begin(512);
+#endif
+  EEPROM.get(calibrationAddr, calibrationValue);  //get stored calibration value from EEPROM
+
+  LoadCell.start(2000, true);
+  if (LoadCell.getTareTimeoutFlag()) {
+    //timeout check wiring
+    Serial.println("T");
+    while (1)
+      ;
+  } else {
+    LoadCell.setCalFactor(calibrationValue);
+    //complete
+    Serial.println(1);
+  }
+}
+
+void readWeight() {
+  static boolean newDataReady = false;
+  const int interval = 0;
+  while (true) {
+    if (LoadCell.update()) {
+      newDataReady = true;
+    }
+    if (newDataReady) {  //for smoothing out load cell data
+      if (millis() > time + interval) {
+        float d = LoadCell.getData();
+        Serial.print("Load cell output value: ");
+        Serial.println(d);
+        newDataReady = 0;
+        time = millis();
+      }
+    }
+  }
+  /*
+  static boolean jump = false;
+  static boolean newDataReady = false;
+  static boolean done = false;
+  const int interval = 0;
+  int readCount = 0;
+  float data = 0;
+  float lastData = LoadCell.getData();
+  while (done == false) {
+    if (LoadCell.update()) {
+      newDataReady = true;
+    }
+    float value;
+    if ((millis() > time + interval) && newDataReady) {
+      value = LoadCell.getData();
+      Serial.println(value);
+      if (jump == true) {
+        if (value - lastData <= 0.01) {
+          for (int i = 0; i < 10; i++) {
+            data += value;
+            readCount++;
+          }
+          done = true;
+        }
+        lastData = value;
+      }
+      if (value - lastData >= 0.1 && jump == false) {
+        jump = true;
+        lastData = value;
+      }
+      newDataReady = false;
+      time = millis();
+    }
+  }
+  Serial.println(data/10);
+  */
 }
 
 /*
@@ -176,14 +270,18 @@ void updateTarget(unsigned char flag, unsigned char canister) {
       ajStepper.move(MANUAL_STEPS);
       resetSpeed();
     }
+  } else if (canister == altflag) {
+    if (flag == weightflag) {
+      readWeight();
+    }
   }
 }
 void readInstructions() {
-  for(int i = 0; i < max_operations; i++) {
+  for (int i = 0; i < max_operations; i++) {
     int main = operation_medium_main[i];
     int adjs = operation_medium_adjs[i];
-    
-    if(main == 0 && adjs == 0) {
+
+    if (main == 0 && adjs == 0) {
       //do nothing
     } else {
       mnStepper.move(main);
@@ -200,11 +298,11 @@ void readInstructions() {
 }
 
 void readInstructions2() {
-  for(int i = 0; i < max_operations; i++) {
+  for (int i = 0; i < max_operations; i++) {
     int main = operation_medium_main[i];
     int adjs = operation_medium_adjs[i];
-    
-    if(main == 0 && adjs == 0) {
+
+    if (main == 0 && adjs == 0) {
       //do nothing
     } else {
       mnStepper2.move(main);
@@ -229,6 +327,7 @@ void loop() {
     swapMotor(canister);
     updateTarget(flags, canister);
   }
+  readWeight();
   mnStepper.runSpeedToPosition();
   ajStepper.runSpeedToPosition();
   mnStepper2.runSpeedToPosition();
